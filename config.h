6e1e49dd02d60295a9d6e45d6009448554ffd7da
christianikov <109367483+christianikov@users.noreply.github.com>
2022-07-16 00:41:10 +0800

    Update config.h

--- config.h
+++ config.h
@@ -8,10 +8,10 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 46;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 42;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 42;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 58;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 28;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
