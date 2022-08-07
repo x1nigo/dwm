b2b841bce79db8e9aec8824e3fca263810357c70
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-08-07 23:33:53 +0800

    Update config.h

--- config.h
+++ config.h
@@ -59,8 +59,7 @@ static const Rule rules[] = {
 	 */
     /* class            instance      title           tags mask  isfloating  isterminal  noswallow  monitor */
     { "Gimp",           NULL,         NULL,           0,         0,          0,           0,        -1 },
-    { "firefox",        NULL,         NULL,           0,         0,          0,          -1,        -1 },
-    { "Brave-browser",  NULL,         NULL,           0,         0,          0,          -1,        -1 },
+    { BROWSER,          NULL,         NULL,           0,         0,          0,          -1,        -1 },
     { NULL,             NULL,         "st",           0,         0,          1,           0,        -1 },
     { NULL,             "stFLOAT",    "st",           0,         1,          1,           0,        -1 },
     { NULL,             NULL,         "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
