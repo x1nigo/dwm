5b0922c070a61387d6a13a7a279fced2d11be8aa
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-08-09 17:43:39 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -52,8 +52,8 @@ static const Rule rules[] = {
     /* class            instance      title           tags mask  isfloating  isterminal  noswallow  monitor */
     { "Gimp",           NULL,         NULL,           0,         0,          0,           0,        -1 },
     { BROWSER,          NULL,         NULL,           0,         0,          0,          -1,        -1 },
-    { NULL,             NULL,         "st",           0,         0,          1,           0,        -1 },
-    { NULL,             "stFLOAT",    "st",           0,         1,          1,           0,        -1 },
+    { "st-256color",    NULL,         NULL,           0,         0,          1,           0,        -1 },
+    { "st-256color",    "stFLOAT",    NULL,           0,         1,          1,           0,        -1 },
     { NULL,             NULL,         "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
