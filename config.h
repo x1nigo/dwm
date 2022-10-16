cca18a1dad9592c14d89713b72783d3eb405899e
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-10-16 20:58:14 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -8,12 +8,12 @@
 #define TERMCLASS "st-256color"
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 50;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 56;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 68;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 28;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
