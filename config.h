aa7a4c99ce1c38d032c8cc8d561ce15016f52a78
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-10-16 14:25:44 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -10,25 +10,26 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 28;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 50;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 56;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 68;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 22;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
 /* fonts */
 static const char *fonts[]          = { "Bitstream Vera Sans Mono:regular:pixelsize=14:antialias=true:autohint=true",
-                                        "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
+                                        "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
 /* colors */
 static const char norm_bg[]         = "#000";
 static const char norm_br[]         = "#000";
 static const char font_norm[]       = "#eee";
 static const char font_sel[]        = "#eee";
-static const char main_bg[]         = "#121228";
-static const char main_br[]         = "#750000";
+static const char main_bg[]         = "#121218";
+static const char main_br[]         = "#980000";
 static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
