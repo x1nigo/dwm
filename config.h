536afd5182bb6c688dd161d14e11f1cf7555c31c
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-10-31 20:50:25 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -8,12 +8,12 @@
 #define TERMCLASS "st-256color"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 12;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 18;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 28;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -21,15 +21,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 0;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
 /* fonts */
-static const char *fonts[]          = { "Fira Mono:regular:pixelsize=14:antialias=true:autohint=true",
+static const char *fonts[]          = { "Bitstream Vera Sans Mono:regular:pixelsize=14:antialias=true:autohint=true",
                                         "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 /* colors */
 static const char norm_bg[]         = "#121218";
 static const char norm_br[]         = "#000";
 static const char font_norm[]       = "#eee";
-static const char font_sel[]        = "#eee";
-static const char main_bg[]         = "#121228";
-static const char main_br[]         = "#b8dcff";
+static const char font_sel[]        = "#000";
+static const char main_bg[]         = "#008080";
+static const char main_br[]         = "#008080";
 static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
