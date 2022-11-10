0de3fa7c64d048d3809e2d65c41e892c45ecf81e
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-11-11 00:53:37 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -23,7 +23,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 0;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
 /* fonts */
-static const char *fonts[]          = { "Bitstream Vera Sans Mono:regular:pixelsize=14:antialias=true:autohint=true",
+static const char *fonts[]          = { "Fira Mono:regular:pixelsize=14:antialias=true:autohint=true",
                                         "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
 /* colors */
 static const char norm_bg[]         = "#121218";
@@ -101,8 +101,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[]      = { TERM, NULL };
-static const char *termfloatcmd[] = { TERM, "-n", "stFLOAT", NULL };
 static const char *dmenucmd[]     = { "dmenu_run", NULL };
 static char dmenumon[2]          = "0";
 
@@ -115,24 +113,6 @@ static const char *micdowncmd[]     = { "sh", "-c", "pamixer --default-source -d
 static const char *micmutecmd[]     = { "sh", "-c", "pamixer --default-source -t ; pkill -RTMIN+1 dwmblocks", NULL };
 static const char *blightupcmd[]    = { "sh", "-c", "screenbrightness inc ; pkill -RTMIN+12 dwmblocks", NULL };
 static const char *blightdowncmd[]  = { "sh", "-c", "screenbrightness dec ; pkill -RTMIN+12 dwmblocks", NULL };
-static const char *kbd_blightup[]   = { "sh", "-c", "kbdbrightness inc", NULL };
-static const char *kbd_blightdown[] = { "sh", "-c", "kbdbrightness dec", NULL };
-
-/* other */
-static const char *browsercmd[] = { BROWSER, NULL };
-static const char *syscmd[]     = { "sh", "-c", "systemize", NULL };
-static const char *scrotcmd[]   = { "sh", "-c", "screenshot", NULL };
-static const char *lfcmd[]      = { TERM, "-e", FILEMANAGER, NULL };
-static const char *texcmd[]     = { "sh", "-c", "texfind", NULL };
-static const char *recordcmd[]  = { "sh", "-c", "recordscreen", NULL };
-static const char *killrecord[] = { "sh", "-c", "killrecording", NULL };
-static const char *emojicmd[]   = { "sh", "-c", "dmenumoji", NULL };
-static const char *webcamcmd[]  = { TERM, "-n", "stFLOAT", "-e", "sh", "-c", "webcam", NULL };
-static const char *musiccmd[]   = { TERM, "-e", MPDClient, NULL };
-static const char *newscmd[]    = { TERM, "-e", RSSReader, NULL };
-static const char *bgcmd[]      = { "sh", "-c", "setwallpaper -s", NULL };
-static const char *bmkcmd[]     = { "sh", "-c", "get-bookmark", NULL };
-static const char *addbmkcmd[]  = { "sh", "-c", "bookmark-it", NULL };
 
 #include <X11/XF86keysym.h>
 #include "movestack.c"
@@ -140,7 +120,7 @@ static const char *addbmkcmd[]  = { "sh", "-c", "bookmark-it", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          SHCMD(TERM) },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -183,17 +163,19 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
 	/* media controls */
-	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
-	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
-	{ 0,                            XF86XK_AudioMute,        spawn, {.v = mutecmd } },
-	{ ShiftMask,                    XF86XK_AudioRaiseVolume, spawn, {.v = micupcmd } },
-	{ ShiftMask,                    XF86XK_AudioLowerVolume, spawn, {.v = micdowncmd } },
-	{ 0,                            XF86XK_AudioMicMute,     spawn, {.v = micmutecmd } },
-	{ 0,                            XF86XK_MonBrightnessUp,  spawn, {.v = blightupcmd } },
-	{ 0,                            XF86XK_MonBrightnessDown, spawn, {.v = blightdowncmd } },
-	{ 0,                            XF86XK_KbdBrightnessUp,  spawn, {.v = kbd_blightup } },
-	{ 0,                            XF86XK_KbdBrightnessDown, spawn, {.v = kbd_blightdown } },
+	{ 0,                            XF86XK_AudioRaiseVolume,      spawn,      {.v = volupcmd } },
+	{ 0,                            XF86XK_AudioLowerVolume,      spawn,      {.v = voldowncmd } },
+	{ 0,                            XF86XK_AudioMute,             spawn,      {.v = mutecmd } },
+	{ ShiftMask,                    XF86XK_AudioRaiseVolume,      spawn,      {.v = micupcmd } },
+	{ ShiftMask,                    XF86XK_AudioLowerVolume,      spawn,      {.v = micdowncmd } },
+	{ 0,                            XF86XK_AudioMicMute,          spawn,      {.v = micmutecmd } },
+	{ 0,                            XF86XK_MonBrightnessUp,       spawn,      {.v = blightupcmd } },
+	{ 0,                            XF86XK_MonBrightnessDown,     spawn,      {.v = blightdowncmd } },
+	{ 0,                            XF86XK_KbdBrightnessUp,       spawn,      SHCMD("kbdbrightness inc") },
+	{ 0,                            XF86XK_KbdBrightnessDown,     spawn,      SHCMD("kbdbrightness dec") },
+
 	/* alternative media controls */
 	{ MODKEY,                       XK_minus,                spawn, {.v = volupcmd } },
 	{ MODKEY|ShiftMask,             XK_minus,                spawn, {.v = voldowncmd } },
@@ -203,22 +185,24 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_BackSpace,            spawn, {.v = micdowncmd } },
 	{ MODKEY,                       XK_bracketright,         spawn, {.v = blightupcmd } },
 	{ MODKEY,                       XK_bracketleft,          spawn, {.v = blightdowncmd } },
-	{ MODKEY|ShiftMask,             XK_d,                    spawn, {.v = killrecord } },
+
 	/* user-added controls */
-	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
-	{ MODKEY,                       XK_q,      spawn,          {.v = syscmd } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = scrotcmd } },
-	{ MODKEY,                       XK_r,      spawn,          {.v = lfcmd } },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = texcmd } },
-	{ MODKEY|ShiftMask,             XK_x,      spawn,          {.v = recordcmd } },
-	{ MODKEY|ControlMask,           XK_Return, spawn,          {.v = termfloatcmd } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = emojicmd } },
-	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webcamcmd } },
-	{ MODKEY|ControlMask,           XK_m,      spawn,          {.v = musiccmd } },
-	{ MODKEY|ShiftMask,             XK_n,      spawn,          {.v = newscmd } },
-        { MODKEY,                       XK_x,      spawn,          {.v = bgcmd } },
-        { MODKEY,                       XK_b,      spawn,          {.v = bmkcmd } },
-        { MODKEY|ControlMask,           XK_b,      spawn,          {.v = addbmkcmd } },
+	{ MODKEY|ControlMask,           XK_w,      spawn,          {.v = (const char*[]){ TERM, "-n", "stFLOAT", "-e", "webcam" } } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = (const char*[]){ TERM, "-e", FILEMANAGER } } },
+	{ MODKEY,                       XK_w,      spawn,          SHCMD(BROWSER) },
+	{ MODKEY,                       XK_q,      spawn,          SHCMD("systemize") },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("screenshot") },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("texfind") },
+	{ MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("recordscreen") },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          SHCMD("killrecording") },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("dmenumoji") },
+	{ MODKEY|ControlMask,           XK_m,      spawn,          {.v = (const char*[]){ TERM, "-e", MPDClient } } },
+	{ MODKEY|ControlMask,           XK_n,      spawn,          {.v = (const char*[]){ TERM, "-e", RSSReader } } },
+        { MODKEY,                       XK_x,      spawn,          SHCMD("setwallpaper -s") },
+        { MODKEY,                       XK_b,      spawn,          SHCMD("get-bookmark") },
+        { MODKEY|ControlMask,           XK_b,      spawn,          SHCMD("bookmark-it") },
+	{ MODKEY|ControlMask,           XK_Return, spawn,          {.v = (const char*[]){ TERM, "-n", "stFLOAT" } } },
+
 	/* tags */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
