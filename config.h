ced12f6881fc0765d1332179dc24d0994926895d
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-11-15 20:38:22 +0800

    Update config.h

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ static const int user_bh            = 0;       /* 0 means that dwm will calculat
 static const char *fonts[]          = { "Liberation Mono:regular:pixelsize=14:antialias=true:autohint=true",
                                         "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
 /* colors */
-static const char norm_bg[]         = "#282828";
+static const char norm_bg[]         = "#1d2021";
 static const char norm_br[]         = "#000";
 static const char font_norm[]       = "#ebdbb2";
 static const char font_sel[]        = "#000";
