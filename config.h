af295dd46c097de505aaade88e8da4672a47b69f
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-04-16 12:35:53 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -8,27 +8,27 @@
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 0;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=8:antialias=true:autohint=true" };
-static const char normbgcolor[]           = "#0a0f14";
-static const char normbordercolor[]       = "#444444";
-static const char normfgcolor[]           = "#ebdbb2";
-static const char selfgcolor[]            = "#ebdbb2";
-static const char selbordercolor[]        = "#880000";
-static const char selbgcolor[]            = "#005577";
+static unsigned int borderpx  = 3;        /* border pixel of windows */
+static unsigned int snap      = 32;       /* snap pixel */
+static unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static int showbar            = 1;        /* 0 means no bar */
+static int topbar             = 1;        /* 0 means bottom bar */
+static int user_bh            = 0;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=8:antialias=true:autohint=true" };
+static char normbgcolor[]           = "#0a0f14";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#ebdbb2";
+static char selfgcolor[]            = "#ebdbb2";
+static char selbordercolor[]        = "#880000";
+static char selbgcolor[]            = "#005577";
 static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
+static char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor, selbordercolor   },
@@ -51,14 +51,14 @@ static const Rule rules[] = {
     { "Gimp",           NULL,         NULL,           0,         0,          0,           0,        -1 },
     { BROWSER,          NULL,         NULL,           0,         0,          0,          -1,        -1 },
     { TERMCLASS,        NULL,         NULL,           0,         0,          1,           0,        -1 },
-    { TERMCLASS,        "floast",    NULL,           0,         1,          1,           0,        -1 },
+    { TERMCLASS,        "floast",     NULL,           0,         1,          1,           0,        -1 },
     { NULL,             NULL,         "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
@@ -111,6 +111,31 @@ static const char *blightdowncmd[]  = { "sh", "-c", "sbright dec ; kill -46 $(pi
 
 #include <X11/XF86keysym.h>
 #include "movestack.c"
+
+/*
+ * Xresources preferences to load at startup
+ */
+ ResourcePref resources[] = {
+ 		{ "font",               STRING,  &fonts },
+ 		{ "normbgcolor",        STRING,  &normbgcolor },
+ 		{ "normbordercolor",    STRING,  &normbordercolor },
+ 		{ "normfgcolor",        STRING,  &normfgcolor },
+ 		{ "selbgcolor",         STRING,  &selbgcolor },
+ 		{ "selbordercolor",     STRING,  &selbordercolor },
+ 		{ "selfgcolor",         STRING,  &selfgcolor },
+ 		{ "borderpx",          	INTEGER, &borderpx },
+ 		{ "snap",          	INTEGER, &snap },
+ 		{ "showbar",          	INTEGER, &showbar },
+ 		{ "topbar",          	INTEGER, &topbar },
+ 		{ "nmaster",          	INTEGER, &nmaster },
+ 		{ "resizehints",       	INTEGER, &resizehints },
+ 		{ "mfact",      	FLOAT,   &mfact },
+		{ "gappih",             INTEGER, &gappih },
+		{ "gappiv",             INTEGER, &gappiv },
+		{ "gappoh",             INTEGER, &gappoh },
+		{ "gappov",             INTEGER, &gappov },
+};
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
