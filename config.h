ff9e666ae63173f933832d9717261ad193fb3e5c
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-18 03:10:38 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -1,33 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMCLASS "St"
+#define TERMCLASS "st-256color"
 #define TERMINAL "st"
 #define BROWSER "chromium"
 #define STATUSBAR "dwmblocks"
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=8:antialias=true:autohint=true" };
-static char normbgcolor[]           = "#1d2021";
-static char normbordercolor[]       = "#222222";
-static char normfgcolor[]           = "#eeeeee";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#500000";
-static char selbgcolor[]            = "#1d2021";
-static const unsigned int baralpha = 0xef;
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:pixelsize=14", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
+static const char normbgcolor[]           = "#282c34";
+static const char normbordercolor[]       = "#282c34";
+static const char normfgcolor[]           = "#d7d7d7";
+static const char selfgcolor[]            = "#1c1f24";
+static const char selbordercolor[]        = "#500000";
+static const char selbgcolor[]            = "#008080";
+static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
-static char *colors[][3]      = {
+static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor, selbordercolor   },
@@ -39,8 +39,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "🦊", "🎯", "📚", "👻", "🤖", "💰", "💵", "🐧", "💀" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -55,9 +54,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
@@ -111,24 +110,6 @@ static const char *blightdowncmd[]  = { "sh", "-c", "xbacklight -dec 5; kill -46
 
 #include <X11/XF86keysym.h>
 #include "movestack.c"
-
-/*
- * Xresources preferences to load at startup
- */
- ResourcePref resources[] = {
- 		{ "normbgcolor",        STRING,  &normbgcolor },
- 		{ "normbordercolor",    STRING,  &normbordercolor },
- 		{ "normfgcolor",        STRING,  &normfgcolor },
- 		{ "selbgcolor",         STRING,  &selbgcolor },
- 		{ "selbordercolor",     STRING,  &selbordercolor },
- 		{ "selfgcolor",         STRING,  &selfgcolor },
- 		{ "borderpx",          	INTEGER, &borderpx },
-		{ "gappih",             INTEGER, &gappih },
-		{ "gappiv",             INTEGER, &gappiv },
-		{ "gappoh",             INTEGER, &gappoh },
-		{ "gappov",             INTEGER, &gappov },
-};
-
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
--- dwm.c
+++ dwm.c
@@ -36,7 +36,6 @@
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
-#include <X11/Xresource.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
 #endif /* XINERAMA */
@@ -160,19 +159,6 @@ typedef struct {
 	int monitor;
 } Rule;
 
-/* Xresources preferences */
-enum resource_type {
-	STRING = 0,
-	INTEGER = 1,
-	FLOAT = 2
-};
-
-typedef struct {
-	char *name;
-	enum resource_type type;
-	void *dst;
-} ResourcePref;
-
 /* function declarations */
 static void applyrules(Client *c);
 static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact);
@@ -276,8 +262,6 @@ static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void xinitvisual();
 static void zoom(const Arg *arg);
-static void load_xresources(void);
-static void resource_load(XrmDatabase db, char *name, enum resource_type rtype, void *dst);
 
 static pid_t getparentprocess(pid_t p);
 static int isdescprocess(pid_t p, pid_t c);
@@ -928,10 +912,6 @@ drawbar(Monitor *m)
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
- 		if (occ & 1 << i)
- 			drw_rect(drw, x + boxs, boxs, boxw, boxw,
- 				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
- 				urg & 1 << i);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
@@ -2730,60 +2710,6 @@ zoom(const Arg *arg)
 	pop(c);
 }
 
-void
-resource_load(XrmDatabase db, char *name, enum resource_type rtype, void *dst)
-{
-	char *sdst = NULL;
-	int *idst = NULL;
-	float *fdst = NULL;
-
-	sdst = dst;
-	idst = dst;
-	fdst = dst;
-
-	char fullname[256];
-	char *type;
-	XrmValue ret;
-
-	snprintf(fullname, sizeof(fullname), "%s.%s", "dwm", name);
-	fullname[sizeof(fullname) - 1] = '\0';
-
-	XrmGetResource(db, fullname, "*", &type, &ret);
-	if (!(ret.addr == NULL || strncmp("String", type, 64)))
-	{
-		switch (rtype) {
-		case STRING:
-			strcpy(sdst, ret.addr);
-			break;
-		case INTEGER:
-			*idst = strtoul(ret.addr, NULL, 10);
-			break;
-		case FLOAT:
-			*fdst = strtof(ret.addr, NULL);
-			break;
-		}
-	}
-}
-
-void
-load_xresources(void)
-{
-	Display *display;
-	char *resm;
-	XrmDatabase db;
-	ResourcePref *p;
-
-	display = XOpenDisplay(NULL);
-	resm = XResourceManagerString(display);
-	if (!resm)
-		return;
-
-	db = XrmGetStringDatabase(resm);
-	for (p = resources; p < resources + LENGTH(resources); p++)
-		resource_load(db, p->name, p->type, p->dst);
-	XCloseDisplay(display);
-}
-
 int
 main(int argc, char *argv[])
 {
@@ -2798,8 +2724,6 @@ main(int argc, char *argv[])
 	if (!(xcon = XGetXCBConnection(dpy)))
 		die("dwm: cannot get xcb connection\n");
 	checkotherwm();
-	XrmInitialize();
-	load_xresources();
 	setup();
 #ifdef __OpenBSD__
 	if (pledge("stdio rpath proc exec ps", NULL) == -1)
