eebf0ca0c557483e015fdd3c844867359285a9f1
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-20 05:34:21 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -3,28 +3,28 @@
 /* Constants */
 #define TERMCLASS "st-256color"
 #define TERMINAL "st"
-#define BROWSER "chromium"
+#define BROWSER "surf"
 #define STATUSBAR "dwmblocks"
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 7;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 7;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 7;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:pixelsize=14", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=8:antialias=true:autohint=true" };
 static const char normbgcolor[]           = "#1d2021";
 static const char normbordercolor[]       = "#282c34";
 static const char normfgcolor[]           = "#d7d7d7";
-static const char selfgcolor[]            = "#1c1f24";
+static const char selfgcolor[]            = "#d7d7d7";
 static const char selbordercolor[]        = "#500000";
-static const char selbgcolor[]            = "#3071db";
+static const char selbgcolor[]            = "#1d2021";
 static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -39,7 +39,10 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
+
+/* alternative tags */
+static const char *tags[] = { "🦊", "🎯", "📚", "🎴", "🤖", "💰", "💵", "🐧", "💀" };
 
 static const Rule rules[] = {
 	/* xprop(1):
