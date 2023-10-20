82c5a3bbfcd60d02babfdc034504c50312d4604c
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-21 01:56:51 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -8,12 +8,12 @@
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 7;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 7;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 7;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -23,7 +23,7 @@ static const char normbgcolor[]           = "#1d2021";
 static const char normbordercolor[]       = "#282c34";
 static const char normfgcolor[]           = "#d7d7d7";
 static const char selfgcolor[]            = "#d7d7d7";
-static const char selbordercolor[]        = "#a9a1e1";
+static const char selbordercolor[]        = "#500000";
 static const char selbgcolor[]            = "#1d2021";
 static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
