afb16f7f82b97928c89374e00ae6a27187a455ab
Chris Inigo <christian.brodeth@gmail.com>
2023-11-04 06:02:34 +0800

    minor changes

--- config.h
+++ config.h
@@ -11,26 +11,26 @@
 #endif
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=8:antialias=true:autohint=true" };
 static const char normbgcolor[]     = "#1d2021";
-static const char normbordercolor[] = "#282828";
+static const char normbordercolor[] = "#1d2837";
 static const char normfgcolor[]     = "#d7d7d7";
 static const char selfgcolor[]      = "#d7d7d7";
-static const char selbordercolor[]  = "#d7d7d7";
-static const char selbgcolor[]      = "#1d2837";
+static const char selbordercolor[]  = "#500817";
+static const char selbgcolor[]      = "#1d2832";
 /* separate title colors */
 static const char tlfgcolor[]       = "#d7d7d7";
-static const char tlbgcolor[]       = "#1d2021";
+static const char tlbgcolor[]       = "#1d2832";
 static const char tlbordercolor[]   = "#1d2021";
 /* transparency or opacity */
 static const unsigned int baralpha = 0xff;
