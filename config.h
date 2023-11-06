57895f6b7063c633f71640d0ca1b1de4b60d53b7
Chris Inigo <christian.brodeth@gmail.com>
2023-11-07 03:08:58 +0800

    changed gaps and window title position

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* Constants */
 #define TERMCLASS "St"
 #define TERM "st"
-#define BROWSER "firefox"
+#define BROWSER "chromium"
 #define SESSION_FILE "/tmp/dwm-session"
 
 #ifndef __OpenBSD__
@@ -13,10 +13,10 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -26,15 +26,15 @@ static const char normbgcolor[]     = "#1d2021";
 static const char normbordercolor[] = "#1d2837";
 static const char normfgcolor[]     = "#d7d7d7";
 static const char selfgcolor[]      = "#d7d7d7";
-static const char selbordercolor[]  = "#500817";
-static const char selbgcolor[]      = "#1d2832";
+static const char selbordercolor[]  = "#500000";
+static const char selbgcolor[]      = "#1d2021";
 /* transparency or opacity */
-static const unsigned int baralpha = 0xff;
+static const unsigned int baralpha    = 0xff;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
-	/*               fg             bg           border   */
-	[SchemeNorm]   = { normfgcolor, normbgcolor, normbordercolor },
-	[SchemeSel]    = { selfgcolor,  selbgcolor, selbordercolor   },
+static const char *colors[][3]        = {
+	/*               fg           bg           border   */
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border     */
