45be6d419345c8820edfc6666b71beb8fc23a8b2
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-06-07 02:07:47 +0800

    Update config.h

--- config.h
+++ config.h
@@ -135,10 +135,10 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
  	{ MODKEY|ControlMask,       	XK_comma,  cyclelayout,    {.i = -1 } },
  	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-    { MODKEY,                       XK_apostrophe, spawn,      {.v = (const char*[]){TERM, "-n", "termfloat", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-l", NULL} } },
+        { MODKEY,                       XK_apostrophe, spawn,      {.v = (const char*[]){TERM, "-n", "termfloat", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-l", NULL} } },
 	{ MODKEY,                       XK_Insert, spawn,          SHCMD("inserter") },
 	{ MODKEY,                       XK_BackSpace, spawn,       SHCMD("sysmenu") },
-    { MODKEY,                       XK_grave,  spawn,          SHCMD("dmenumoji") },
+        { MODKEY,                       XK_grave,  spawn,          SHCMD("dmenumoji") },
 	{ MODKEY|ControlMask,           XK_k,      incrgaps,       {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_j,      incrgaps,       {.i = -1 } },
 	{ MODKEY,                       XK_g,      togglegaps,     {0} },
@@ -151,7 +151,7 @@ static const Key keys[] = {
 	{ 0, XF86XK_MonBrightnessUp,    spawn,     {.v = (const char*[]){ "xbacklight", "-inc", "5", NULL } } },
 	{ 0, XF86XK_MonBrightnessDown,  spawn,     {.v = (const char*[]){ "xbacklight", "-dec", "5", NULL } } },
 #endif
-#ifndef __FreeBSD__
+#ifdef __FreeBSD__
 	{ 0, XF86XK_AudioRaiseVolume,   spawn,     SHCMD("mixer vol=+.05") },
 	{ 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("mixer vol=-.05") },
 	{ 0, XF86XK_MonBrightnessUp,    spawn,     SHCMD("backlight incr 5") },
