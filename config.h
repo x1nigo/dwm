c01b7a1e6f155cdd2d6e246342eb78403d9d5b69
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-06-18 18:49:01 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* Constants */
 #define TERMCLASS "St"
 #define TERM "st"
-#define BROWSER "chromium"
+#define BROWSER "ungoogled-chromium"
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
@@ -150,6 +150,10 @@ static const Key keys[] = {
 	{ 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-") },
 	{ 0, XF86XK_MonBrightnessUp,    spawn,     {.v = (const char*[]){ "xbacklight", "-inc", "5", NULL } } },
 	{ 0, XF86XK_MonBrightnessDown,  spawn,     {.v = (const char*[]){ "xbacklight", "-dec", "5", NULL } } },
+#else
+	{ 0, XF86XK_AudioMute,          spawn,     SHCMD("ref-bar") },
+	{ 0, XF86XK_AudioRaiseVolume,   spawn,     SHCMD("ref-bar") },
+	{ 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("ref-bar") },
 #endif
 #ifdef __FreeBSD__
 	{ 0, XF86XK_AudioRaiseVolume,   spawn,     SHCMD("mixer vol=+.05") },
