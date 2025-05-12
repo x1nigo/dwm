5aff15b3edcb5f982a68c5d004ed40c5fb6dcbaa
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-05-12 17:23:30 +0800

    Update config.h

--- config.h
+++ config.h
@@ -19,11 +19,11 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=8" };
-static const char normbgcolor[]     = "#111111";
-static const char normbordercolor[] = "#222222";
+static const char normbgcolor[]     = "#1d2021";
+static const char normbordercolor[] = "#282828";
 static const char normfgcolor[]     = "#ebdbb2";
 static const char selfgcolor[]      = "#ebdbb2";
-static const char selbordercolor[]  = "#570000";
+static const char selbordercolor[]  = "#0f2837";
 static const char selbgcolor[]      = "#0f2837";
 static const unsigned int baralpha = 0xef;
 static const unsigned int borderalpha = OPAQUE;
