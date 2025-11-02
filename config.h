1ef5985cdb4931d9ae2f60ee1d08e6d760949c84
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-11-02 10:44:30 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -11,16 +11,16 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 0;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 24;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "monospace:size=10:antialias=true",
-	"NotoColorEmoji:size=8:antialias=true:autohint=true",
-	"Mononoki Nerd Font:size=9:antialias=true:autohint=true" };
+	"NotoColorEmoji:size=8:antialias=true",
+	"Mononoki Nerd Font:size=8:antialias=true" };
 static const char normbgcolor[]     = "#21242b";
 static const char normbordercolor[] = "#282828";
 static const char normfgcolor[]     = "#d7d7d7";
@@ -42,7 +42,7 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "", "", "", "󰼭", "", "󰈈", "󰽰", "", "󰚌", "" };
+static const char *tags[] = { "", "", "", "󰼭", "", "󰈈", "", "", "󰣘" };
 
 static const Rule rules[] = {
 	/* xprop(1):
