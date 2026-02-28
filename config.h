f166097d545d50d2b4e274b09bea36629e765641
x1nigo <109367483+x1nigo@users.noreply.github.com>
2026-03-01 00:05:15 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 #define MSCPLAYER "ncmpcpp"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -18,14 +18,14 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=8" };
-static unsigned int baralpha        = 0xef;
+static unsigned int baralpha        = 0xe7;
 static unsigned int borderalpha     = OPAQUE;
 static const char normbgcolor[]     = "#1d2021";
 static const char normbordercolor[] = "#282828";
 static const char normfgcolor[]     = "#ebdbb2";
 static const char selfgcolor[]      = "#ebdbb2";
-static const char selbgcolor[]      = "#005577";
-static const char selbordercolor[]  = "#005577";
+static const char selbgcolor[]      = "#0f2737";
+static const char selbordercolor[]  = "#570000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm]  = { normfgcolor,  normbgcolor, normbordercolor },
